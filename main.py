3a21925635b9e7914906725e5a02780ca6af8547
aporria <aporria0451@gmail.com>
2022-05-13 19:00:43 -0700

    added adjacency matrix methods

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+import sys
+
+from mpi4py import MPI
+import numpy as np
+import networkx as nx
+import scipy as sp
+
+
 def main():
-    print('Hello world')
+    #facebook_combined.txt
+    np.set_printoptions(threshold=sys.maxsize)
+    file = input("Enter name of file to analyze: ")
+    digraph = nx.read_edgelist(file, create_using=nx.DiGraph(), nodetype=int, edgetype=int)
+    graph = digraph.to_undirected()
+    #adj_mat = nx.convert.to_dict_of_dicts(digraph)
+    #adj_mat = nx.convert.to_dict_of_lists(digraph)
+    adj_mat = nx.to_numpy_matrix(digraph)
+    print(adj_mat)
+
+
+    def dijkstra_p():
+        comm = MPI.COMM_WORLD
+        size = comm.Get_size()
+        rank = comm.Get_rank()
+
+        def min_dist():
+            # encontra a menor distancia no set local
+            dist = float('inf')
+            pos = -1
+            for index, distance in enumerate(set):
+                if distance < dist:
+                    dist = distance
+                    pos = index
+            return set[pos]
+
+        dist = {}
+        prev = {}
 
 
 if __name__ == '__main__':
